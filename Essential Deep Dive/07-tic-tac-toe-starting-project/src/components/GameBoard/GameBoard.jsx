92543cbc4bfb3368734a623bf7e50355ac3128fc
hwnii <hwniieo@gmail.com>
2024-04-30 00:06:25 +0900

    today last commit

---
+++ Essential Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard/GameBoard.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setGameBoard((prevGameBoard) => {
+    //   const updatedBoard = [
+    //     ...prevGameBoard.map((innerArray) => [...innerArray]),
+    //   ];
+    //   return updatedBoard;
+    // });
+    initialGameBoard[rowIndex][colIndex] = 'X';
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id='game-board'>
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((col, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {col}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+// {
+//   gameBoard.map((row, rowIndex) => (
+//     <li key={rowIndex}>
+//       <ol>
+//         {row.map((playerSymbol, colIndex) => (
+//           <li key={colIndex}>
+//             <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+//               {playerSymbol}
+//             </button>
+//           </li>
+//         ))}
+//       </ol>
+//     </li>
+//   ));
+// }
