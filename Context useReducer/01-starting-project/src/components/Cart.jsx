ef8b88d3b4471efa8b1460b8ac12ddbf888b4ea8
hwnii <hwniieo@gmail.com>
2024-05-16 22:49:32 +0900

    complete

--- Context useReducer/01-starting-project/src/components/Cart.jsx
+++ Context useReducer/01-starting-project/src/components/Cart.jsx
@@ -1,17 +1,22 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from 'react'
+import { CartContext } from '../store/shopping-cart-context'
+
+export default function Cart() {
+  const { items, updateCartItemQuantity } = useContext(CartContext)
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
-    0
-  );
-  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+    0,
+  )
+  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`
 
   return (
-    <div id="cart">
+    <div id='cart'>
       {items.length === 0 && <p>No items in cart!</p>}
       {items.length > 0 && (
-        <ul id="cart-items">
+        <ul id='cart-items'>
           {items.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
+            const formattedPrice = `$${item.price.toFixed(2)}`
 
             return (
               <li key={item.id}>
@@ -19,23 +24,23 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span>{item.name}</span>
                   <span> ({formattedPrice})</span>
                 </div>
-                <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                <div className='cart-item-actions'>
+                  <button onClick={() => updateCartItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateCartItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
               </li>
-            );
+            )
           })}
         </ul>
       )}
-      <p id="cart-total-price">
+      <p id='cart-total-price'>
         Cart Total: <strong>{formattedTotalPrice}</strong>
       </p>
     </div>
-  );
+  )
 }
--- Context useReducer/01-starting-project/src/components/Header.jsx
+++ Context useReducer/01-starting-project/src/components/Header.jsx
@@ -1,17 +1,20 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react'
 
-import CartModal from './CartModal.jsx';
+import CartModal from './CartModal.jsx'
+import { CartContext } from '../store/shopping-cart-context.jsx'
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
-  const modal = useRef();
+export default function Header({ onUpdateCartItemQuantity }) {
+  const { items } = useContext(CartContext)
 
-  const cartQuantity = cart.items.length;
+  const modal = useRef()
+
+  const cartQuantity = items.length
 
   function handleOpenCartClick() {
-    modal.current.open();
+    modal.current.open()
   }
 
-  let modalActions = <button>Close</button>;
+  let modalActions = <button>Close</button>
 
   if (cartQuantity > 0) {
     modalActions = (
@@ -19,21 +22,15 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
         <button>Close</button>
         <button>Checkout</button>
       </>
-    );
+    )
   }
 
   return (
     <>
-      <CartModal
-        ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
-        title="Your Cart"
-        actions={modalActions}
-      />
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
+      <CartModal ref={modal} title='Your Cart' actions={modalActions} />
+      <header id='main-header'>
+        <div id='main-title'>
+          <img src='logo.png' alt='Elegant model' />
           <h1>Elegant Context</h1>
         </div>
         <p>
@@ -41,5 +38,5 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
         </p>
       </header>
     </>
-  );
+  )
 }
