8e3ff982dde238dd17948a6c07a5fe710ebd7b1d
hwnii <hwniieo@gmail.com>
2024-05-17 13:02:29 +0900

    feat: local storage

--- Side Effects useEffect/01-starting-project/src/App.jsx
+++ Side Effects useEffect/01-starting-project/src/App.jsx
@@ -7,11 +7,16 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx'
 import logoImg from './assets/logo.png'
 import { sortPlacesByDistance } from './loc.js'
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id),
+)
+
 function App() {
   const modal = useRef()
   const selectedPlace = useRef()
   const [availablePlaces, setAvailablePlaces] = useState([])
-  const [pickedPlaces, setPickedPlaces] = useState([])
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces)
 
   useEffect(() => {
     // 현재 위치에서 가까운 지역을 우선으로 보여주는 sort 기능 구현
@@ -43,6 +48,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id)
       return [place, ...prevPickedPlaces]
     })
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]))
+    }
   }
 
   function handleRemovePlace() {
@@ -50,6 +60,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current),
     )
     modal.current.close()
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+    localStorage.setItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)),
+    )
   }
 
   return (
